f3bb743c56c8acf8b6124f1fd8c24e18108360b3
gustavoemf <gustavoeduardomf@gmail.com>
2025-10-16 18:45:20 -0300

    implementação regra de negócio create e correção retorno get by id

--- src/main/java/org/gustavomuller/pokedex/rest/PokemonResource.java
+++ src/main/java/org/gustavomuller/pokedex/rest/PokemonResource.java
@@ -1,15 +1,17 @@
 package org.gustavomuller.pokedex.rest;
 
+import io.quarkus.logging.Log;
 import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Response;
 import org.gustavomuller.pokedex.Pokemon;
 import org.gustavomuller.pokedex.service.PokemonService;
 
 import java.util.List;
+
+import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 
 /**
  * API endpoints.
@@ -32,7 +34,15 @@ public class PokemonResource {
 
     @GET
     @Path("/{id}")
-    public Uni<Pokemon> getPokemonById(@PathParam("id") Long id) {
-        return service.findPokemonById(id);
+    public Uni<Response> getPokemonById(@PathParam("id") Long id) {
+        return service.findPokemonById(id)
+                .onItem().ifNotNull().transform(p -> {
+                    Log.debugf("Found pokemon: %s", p);
+                    return Response.ok(p).build();
+                })
+                .replaceIfNullWith(() -> {
+                    Log.debugf("No pokemon found with id %d", id);
+                    return Response.status(Response.Status.NOT_FOUND).build();
+                });
     }
 }
--- src/main/java/org/gustavomuller/pokedex/service/PokemonService.java
+++ src/main/java/org/gustavomuller/pokedex/service/PokemonService.java
@@ -1,9 +1,12 @@
 package org.gustavomuller.pokedex.service;
 
+import io.quarkus.hibernate.reactive.panache.common.WithTransaction;
 import io.quarkus.logging.Log;
 import io.quarkus.panache.common.Sort;
 import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import org.gustavomuller.pokedex.Pokemon;
 
 import java.util.List;
@@ -22,5 +25,11 @@ public class PokemonService {
     public Uni<Pokemon> findPokemonById(Long id) {
         Log.debugf("Getting pokemon by id = %d", id);
         return Pokemon.findById(id);
+    }
+
+    @WithTransaction
+    public Uni<Pokemon> persistPokemon(@NotNull @Valid Pokemon pokemon) {
+        Log.debugf("Persisting pokemon %s", pokemon);
+        return Pokemon.persist(pokemon).replaceWith(pokemon);
     }
 }
