19159f0b397213e356923dd347603bacd2c07450
gustavoemf <gustavoeduardomf@gmail.com>
2025-10-17 10:41:42 -0300

    getters e setters & toString & sprite

--- src/main/java/org/gustavomuller/pokedex/Pokemon.java
+++ src/main/java/org/gustavomuller/pokedex/Pokemon.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Entity;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 /**
  * Entity class for a Pokemon.
@@ -14,6 +15,7 @@ import jakarta.validation.constraints.Positive;
 public class Pokemon extends PanacheEntity {
 
     @NotBlank
+    @Size(min = 3, max = 50)
     @Column(unique = true)
     public String identifier;
 
@@ -29,4 +31,67 @@ public class Pokemon extends PanacheEntity {
     @NotNull
     @Positive
     public Integer totalStats;
+
+    public String sprite;
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public Integer getPokedexNumber() {
+        return pokedexNumber;
+    }
+
+    public void setPokedexNumber(Integer pokedexNumber) {
+        this.pokedexNumber = pokedexNumber;
+    }
+
+    public String getType1() {
+        return type1;
+    }
+
+    public void setType1(String type1) {
+        this.type1 = type1;
+    }
+
+    public String getType2() {
+        return type2;
+    }
+
+    public void setType2(String type2) {
+        this.type2 = type2;
+    }
+
+    public Integer getTotalStats() {
+        return totalStats;
+    }
+
+    public void setTotalStats(Integer totalStats) {
+        this.totalStats = totalStats;
+    }
+
+    public String getSprite() {
+        return sprite;
+    }
+
+    public void setSprite(String sprite) {
+        this.sprite = sprite;
+    }
+
+    @Override
+    public String toString() {
+        return "Hero{" +
+                "id=" + this.id +
+                ", identifier='" + this.identifier + '\'' +
+                ", pokedexNumber='" + this.pokedexNumber + '\'' +
+                ", type1=" + this.type1 +
+                ", type2='" + this.type2 + '\'' +
+                ", totalStatus='" + this.totalStats + '\'' +
+                ", sprite='" + this.sprite + '\'' +
+                '}';
+    }
 }
